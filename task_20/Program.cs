75aae71263e9690f786f9f62b47d14a82c4c5dad
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-24 11:04:07 +0500

    Добавьте файлы проекта.

---
+++ task_20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task_20
+{
+    class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                MyDelegate myDelegate = null;
+                myDelegate += CircleLength;
+                myDelegate += CircleArea;
+                myDelegate += CircleVolume;
+                Console.Write("Введите радиус: r=");
+                try
+                {
+                    myDelegate?.Invoke(Convert.ToDouble(Console.ReadLine()));
+                }
+                catch
+                {
+                    Console.WriteLine("Не удалось распознать радиус.");
+                }
+                Console.WriteLine();
+            }
+        }
+        static double CircleLength(double r)
+        {
+            Console.WriteLine("Длина окружности радиусом {1}: {0}", 2 * Math.PI * r, r);
+            return 2 * Math.PI * r;
+        }
+        static double CircleArea(double r)
+        {
+            Console.WriteLine("Площадь круга радиусом {1}: {0}", Math.PI * r * r, r);
+            return Math.PI * r * r;
+        }
+        static double CircleVolume(double r)
+        {
+            Console.WriteLine("Объем шара радиусом {1}: {0}", (4 / 3) * Math.PI * r * r * r, r);
+            return (4 / 3) * Math.PI * r * r * r;
+        }
+    }
+}
